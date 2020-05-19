a2a18f5293df351b75478bdcb5bda3d4cca86d07
Sam <sam.ninja09@toppertips.com>
2020-05-19 18:53:41 +0530

    data type complete

--- 02-data-type-in-snowflake/data-type.sql
+++ 02-data-type-in-snowflake/data-type.sql
@@ -90,13 +90,58 @@ String Constant
 	- Two single quotes is not the same as the double quote character
 	- $$string with a ' character$$
 	- Note that the string constant cannot contain double-dollar signs. <***IMP***>
+
+Boolean Data Type & Key Note
+	- BOOLEAN can have TRUE/FALSE values.
+	- BOOLEAN can also have an “unknown” value, which is represented by NULL
+	- Boolean Conversionn
+		* String 
+		* true/t/yes/y/on/1 = True
+		* false/f/no/f/off/0 = False
+		*
+		* Numeric
+		* 0 - zero is false
+		* any non zero (+/-) is true
+	- String/Numeric convertionnn
+		* to_string(True) -> true (lower case) -> 1 (for number)
+		* to_string(False) -> false (lower case) -> 0 (for number)
+	- Null is converted to NULL 
+
+Quik Review - Ternary Logic - three-valued logic (3VL)
+	- logic system with three truth values: TRUE, FALSE, and UNKNOWN
+	- Snowflake, UNKNOWN is represented by NULL
+	- When used in expressions (e.g. SELECT list), UNKNOWN results are returned as NULL values.	
+	- When used as a predicate (e.g. WHERE clause), UNKNOWN results evaluate to FALSE.
+
+Date/time key notes
+	- single DATE data type for storing dates (with no time elements)
+	- DATE supported formats for string constants (YYYY-MM-DD, DD-MON-YYYY, etc.)
+	- When a date-only format is used, the associated time is assumed to be midnight on that day.
+	- timestap = combined date + time
+	- DATETIME is an alias for TIMESTAMP_NTZ
+	- all accepted timestamps are valid inputs for dates
+	-TIME data type for storing times in the form of HH:MI:SS
+	- TIME supports an optional precision parameter for fractional seconds, e.g. TIME(3)
+	- Time precision can range from 0 (seconds) to 9 (nanoseconds). The default precision is 9
+	- All TIME values must be between 00:00:00 and 23:59:59.999999999
+	- TIME internally stores “wallclock” time, and all operations on TIME values are performed without taking any time zone into consideration
 */
 
+-- setting the timezone
+alter session set timestamp_type_mapping = timestamp_ntz;
+create or replace table ts_test(ts timestamp);
+desc table ts_test;
 
+create or replace table ts_test2(ts timestamp_ltz);
+desc table ts_test2;
+
+Use TIMESTAMP_LTZ with different time zones:
 /*
 Important links:
 1.https://docs.snowflake.com/en/sql-reference/data-types.html
 2. https://docs.snowflake.com/en/sql-reference/data-types-numeric.html
 3. https://docs.snowflake.com/en/sql-reference/data-types-text.html
 4. https://docs.snowflake.com/en/user-guide/binary-input-output.html
+5. https://docs.snowflake.com/en/sql-reference/data-types-logical.html
+6. https://docs.snowflake.com/en/sql-reference/ternary-logic.html
 */
